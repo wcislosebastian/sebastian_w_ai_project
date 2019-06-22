5e3b4c41695072ecb8cc145e44af0fc3ddebaaf8
Sebastian Wcisło <wcislo.sebastian.krystian@gmail.com>
2019-06-22 11:28:34 +0200

    Added missing default display names

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,8 @@
 class Attendance < ApplicationRecord
   belongs_to :real_class
   belongs_to :student
+
+  def display_name
+     presence
+  end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,8 @@ class Note < ApplicationRecord
   belongs_to :real_class
   belongs_to :teacher
   belongs_to :student
+
+  def display_name
+     note
+  end
 end
