6353d6b95f052695299e1831df6c72aa94c7204d
Sebastian Wcisło <wcislo.sebastian.krystian@gmail.com>
2019-06-22 11:39:39 +0200

    Added registration

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,7 +2,7 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :registerable
   belongs_to :teacher, optional: true
   belongs_to :student, optional: true
   belongs_to :parent, optional: true
